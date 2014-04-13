967160167fe9591588e67f152cc4e6723b2754dc
Joel Friedlaender <joel@redguava.com.au>
2014-04-13 22:39:53 +1000

    added attributes to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140413103356) do
+ActiveRecord::Schema.define(version: 20140413123300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,33 @@ ActiveRecord::Schema.define(version: 20140413103356) do
     t.string   "role"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "attr_strength"
+    t.integer  "attr_strength_epic"
+    t.boolean  "attr_strength_favoured"
+    t.integer  "attr_dexterity"
+    t.integer  "attr_dexterity_epic"
+    t.boolean  "attr_dexterity_favoured"
+    t.integer  "attr_stamina"
+    t.integer  "attr_stamina_epic"
+    t.boolean  "attr_stamina_favoured"
+    t.integer  "attr_charisma"
+    t.integer  "attr_charisma_epic"
+    t.boolean  "attr_charisma_favoured"
+    t.integer  "attr_manipulation"
+    t.integer  "attr_manipulation_epic"
+    t.boolean  "attr_manipulation_favoured"
+    t.integer  "attr_appearance"
+    t.integer  "attr_appearance_epic"
+    t.boolean  "attr_appearance_favoured"
+    t.integer  "attr_perception"
+    t.integer  "attr_perception_epic"
+    t.boolean  "attr_perception_favoured"
+    t.integer  "attr_intelligence"
+    t.integer  "attr_intelligence_epic"
+    t.boolean  "attr_intelligence_favoured"
+    t.integer  "attr_wits"
+    t.integer  "attr_wits_epic"
+    t.boolean  "attr_wits_favoured"
   end
 
 end
