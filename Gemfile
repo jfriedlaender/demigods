81bf216c0eeca3a51fc497f5c4c113264d54a239
Joel Friedlaender <joel@redguava.com.au>
2014-04-13 18:48:29 +1000

    first commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+ruby "2.1.1"
+
+gem 'rails', '4.0.1'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+gem 'rails_12factor', group: :production
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
+
+# Use unicorn as the app server
+# gem 'unicorn'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demigods::Application.config.secret_key_base = 'c12da72dc66f77627dcaa09322a1fdcb5d848f4999318e0a82755768fbf4ecb5ef5d702037c6732d03dedb3ceca807e39d786a7faf21aad29df2839584ed0076'
